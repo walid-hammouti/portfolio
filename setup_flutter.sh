d13ec80e4b7d3d7d6e4f90917aa18793e16bb5a8
walid-hammouti <w.walidcontacts@gmail.com>
2025-10-04 17:22:27 +0100

    Update Flutter SDK installation to version 3.27.4 in setup script

--- setup_flutter.sh
+++ setup_flutter.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -e
 
-# Download Flutter SDK
 echo "Installing Flutter SDK..."
-curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.3-stable.tar.xz
-tar xf flutter_linux_3.24.3-stable.tar.xz
+curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.27.4-stable.tar.xz
+tar xf flutter_linux_3.27.4-stable.tar.xz
 
-# Add Flutter to PATH
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Check Flutter version
 flutter --version
-
-# Enable web support and get packages
 flutter config --enable-web
 flutter pub get
