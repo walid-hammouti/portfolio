dfa7a93d97e173b1173052faf5436c447f1b727a
walid-hammouti <w.walidcontacts@gmail.com>
2025-10-04 17:38:25 +0100

    Update Flutter SDK installation to version 3.29.0 in setup script

--- setup_flutter.sh
+++ setup_flutter.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-# --- Install Flutter SDK ---
-echo "Downloading Flutter SDK..."
-curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.27.4-stable.tar.xz
-tar xf flutter_linux_3.27.4-stable.tar.xz
+# --- Install Flutter SDK (with Dart 3.7.x) ---
+echo "Downloading Flutter SDK (3.29.0-stable)..."
+curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.29.0-stable.tar.xz
+tar xf flutter_linux_3.29.0-stable.tar.xz
 
 # --- Add Flutter to PATH ---
 export PATH="$PATH:`pwd`/flutter/bin"
