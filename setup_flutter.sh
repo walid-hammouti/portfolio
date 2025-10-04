f0545d9f71775efa2de91203fa1c2c5f586cb3b1
walid-hammouti <w.walidcontacts@gmail.com>
2025-10-04 17:19:42 +0100

    Update Flutter SDK installation to version 3.24.3 and enhance setup script output

--- setup_flutter.sh
+++ setup_flutter.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
-# Download Flutter SDK (3.35.5 includes Dart 3.7)
-curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.35.5-stable.tar.xz
-tar xf flutter_linux_3.35.5-stable.tar.xz
-
-# Fix Git safe directory issue
-git config --global --add safe.directory /vercel/path0/flutter
+# Download Flutter SDK
+echo "Installing Flutter SDK..."
+curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.3-stable.tar.xz
+tar xf flutter_linux_3.24.3-stable.tar.xz
 
 # Add Flutter to PATH
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Check Flutter & install dependencies
+# Check Flutter version
 flutter --version
+
+# Enable web support and get packages
+flutter config --enable-web
 flutter pub get
