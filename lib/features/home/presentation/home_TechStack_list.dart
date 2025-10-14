0d50414d671841f049745fd15e9e5b342f0c99be
walid-hammouti <w.walidcontacts@gmail.com>
2025-10-14 22:55:21 +0100

    fix the path of svg icons

--- lib/features/home/presentation/home_TechStack_list.dart
+++ lib/features/home/presentation/home_TechStack_list.dart
@@ -6,33 +6,39 @@ import 'package:portfolio/features/project/presentation/project_item.dart';
 import 'package:portfolio/widgets/home_title_subtitle.dart';
 
 final techStackItems = [
-  {"image": "icons/tech_stack/dart.svg", "title": "dart"},
-  {"image": "icons/tech_stack/flutter.svg", "title": "flutter"},
+  {"image": "assets/icons/tech_stack/dart.svg", "title": "dart"},
+  {"image": "assets/icons/tech_stack/flutter.svg", "title": "flutter"},
 
-  {"image": "icons/tech_stack/folder-type-cubit-opened.svg", "title": "cubit"},
-  {"image": "icons/tech_stack/riverpod.svg", "title": "riverpod"},
+  {
+    "image": "assets/icons/tech_stack/folder-type-cubit-opened.svg",
+    "title": "cubit",
+  },
+  {"image": "assets/icons/tech_stack/riverpod.svg", "title": "riverpod"},
 
-  {"image": "icons/tech_stack/supabase-icon.svg", "title": "supabase"},
-  {"image": "icons/tech_stack/firebase.svg", "title": "firebase"},
-  {"image": "icons/tech_stack/appwrite.svg", "title": "app write"},
-  {"image": "icons/tech_stack/vercel-light.svg", "title": "vercel"},
+  {"image": "assets/icons/tech_stack/supabase-icon.svg", "title": "supabase"},
+  {"image": "assets/icons/tech_stack/firebase.svg", "title": "firebase"},
+  {"image": "assets/icons/tech_stack/appwrite.svg", "title": "app write"},
+  {"image": "assets/icons/tech_stack/vercel-light.svg", "title": "vercel"},
 
-  {"image": "icons/tech_stack/python.svg", "title": "python"},
-  {"image": "icons/tech_stack/matplotlib.svg", "title": "matplotlib"},
-  {"image": "icons/tech_stack/numpy.svg", "title": "numpy"},
-  {"image": "icons/tech_stack/pandas.svg", "title": "pandas"},
-  {"image": "icons/tech_stack/scikitlearn.svg", "title": "scikit-learn"},
-  {"image": "icons/tech_stack/vscode.svg", "title": "vscode"},
-  {"image": "icons/tech_stack/androidstudio.svg", "title": "android studio"},
-  {"image": "icons/tech_stack/git-icon.svg", "title": "git"},
-  {"image": "icons/tech_stack/github-light.svg", "title": "github"},
-  {"image": "icons/tech_stack/linux.svg", "title": "linux"},
-  {"image": "icons/tech_stack/postman-icon.svg", "title": "postman"},
-  {"image": "icons/tech_stack/bash-icon.svg", "title": "bash"},
-  {"image": "icons/tech_stack/c.svg", "title": "c"},
-  {"image": "icons/tech_stack/gtk-light.svg", "title": "gtk"},
-  {"image": "icons/tech_stack/java.svg", "title": "java"},
-  {"image": "icons/tech_stack/jira.svg", "title": "jira"},
+  {"image": "assets/icons/tech_stack/python.svg", "title": "python"},
+  {"image": "assets/icons/tech_stack/matplotlib.svg", "title": "matplotlib"},
+  {"image": "assets/icons/tech_stack/numpy.svg", "title": "numpy"},
+  {"image": "assets/icons/tech_stack/pandas.svg", "title": "pandas"},
+  {"image": "assets/icons/tech_stack/scikitlearn.svg", "title": "scikit-learn"},
+  {"image": "assets/icons/tech_stack/vscode.svg", "title": "vscode"},
+  {
+    "image": "assets/icons/tech_stack/androidstudio.svg",
+    "title": "android studio",
+  },
+  {"image": "assets/icons/tech_stack/git-icon.svg", "title": "git"},
+  {"image": "assets/icons/tech_stack/github-light.svg", "title": "github"},
+  {"image": "assets/icons/tech_stack/linux.svg", "title": "linux"},
+  {"image": "assets/icons/tech_stack/postman-icon.svg", "title": "postman"},
+  {"image": "assets/icons/tech_stack/bash-icon.svg", "title": "bash"},
+  {"image": "assets/icons/tech_stack/c.svg", "title": "c"},
+  {"image": "assets/icons/tech_stack/gtk-light.svg", "title": "gtk"},
+  {"image": "assets/icons/tech_stack/java.svg", "title": "java"},
+  {"image": "assets/icons/tech_stack/jira.svg", "title": "jira"},
 ];
 
 class HomeTechstackList extends StatelessWidget {
